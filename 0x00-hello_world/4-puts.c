60c1508bef283614462bb1cf25a697fd2ad2dd5d
Giftedlove <lovedaniel0813@gmail.com>
2023-08-10 13:30:09 -0700

    My four puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main (){ 
-	puts("Programming is like building a multilingual puzzle";
+int main () {
+	puts("Programming is like building a multilingual puzzle");
 
 	return 0;
-}
+
+}	
+
