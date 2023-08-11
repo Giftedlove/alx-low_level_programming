2b41c21a8746f77baf22539a294070cacaf6adfa
Giftedlove <lovedaniel0813@gmail.com>
2023-08-10 22:02:08 -0700

    My hundred and one C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * Return: 1
  */
 int main(void)
-{	 char *text= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	 char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
         write(2, text, strlen(text));
         return(1);
 }	
