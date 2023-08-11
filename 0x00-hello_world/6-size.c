38164aac5eade4766cd4e566a5d09211656fdae8
Giftedlove <lovedaniel0813@gmail.com>
2023-08-10 18:05:29 -0700

    My six size C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include<stdio.h>
+int main(void)
+{	printf("Size of an int: %d byte(S)\n", sizeof(int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a cihar: %d byte(s)\n", sizeof(char));
+}	return (0);
