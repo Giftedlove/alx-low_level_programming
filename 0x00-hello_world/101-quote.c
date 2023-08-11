f289b0586a52557a172fae90c29fbdd363dbcfd4
Giftedlove <lovedaniel0813@gmail.com>
2023-08-10 21:51:11 -0700

    My hundred and one C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - It's a program that prints a line to the standard error
+ * Return: 1
  */
 int main(void)
-{       
-        write(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
-	return(1);
-}
+{	 char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write(2, text, strlen(text));
+        return (1);
+}	
