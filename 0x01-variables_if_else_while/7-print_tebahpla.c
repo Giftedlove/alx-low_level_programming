9fd995c54c3a26e7f0d4b37f62bb98cf2c036157
Giftedlove <lovedaniel0813@gmail.com>
2023-08-11 21:45:52 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the lowercase alphabet in reverse
+ *main - program that prints the lowercase alphabet in reverse
  *followed by a new line
  * Return: 0
  */
