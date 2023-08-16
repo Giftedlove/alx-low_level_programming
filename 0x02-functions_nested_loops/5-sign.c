27514dee0c7ff93931297be19ebede2577a025d9
Giftedlove <lovedaniel0813@gmail.com>
2023-08-16 06:22:23 -0700

    Return Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: sign of number to be checked
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+	return (0);
+}
