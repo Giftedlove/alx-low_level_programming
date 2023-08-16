f154484336d04fe04ac2f71ebaae369c99363aca
Giftedlove <lovedaniel0813@gmail.com>
2023-08-16 12:24:13 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,28 @@
 void times_table(void)
 {
 	int i;
+	int j;
 	int result;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		result = 9 * i;
-		_putchar('9');
-		_putchar(' ');
-		_putchar('x');
-		_putchar(' ');
-		_putchar(i + '0');
-		_putchar(' ');
-		_putchar('=');
-		_putchar(' ');
-		_putchar(result + '0');
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			result = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (result <= 9)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
 		_putchar('\n');
 	}
 }
