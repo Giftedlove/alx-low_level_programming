6af448338032873906d02eba7eff18674a674fa8
Giftedlove <lovedaniel0813@gmail.com>
2023-09-12 14:06:51 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define dog
+ * @name: the name given
+ * @age: the age given
+ * @owner: the owner
+ * Desscription: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
